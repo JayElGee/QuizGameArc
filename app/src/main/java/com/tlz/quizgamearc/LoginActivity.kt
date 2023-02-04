fc60e15b53d34ae6402dbc4fdb41ac3abf8e5572
TheLordZedd <pcgpod@yahoo.com>
2023-02-03 23:59:23 -0600

    Starting sign up page implementation

--- app/src/main/java/com/tlz/quizgamearc/LoginActivity.kt
+++ app/src/main/java/com/tlz/quizgamearc/LoginActivity.kt
@@ -2,10 +2,35 @@ package com.tlz.quizgamearc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tlz.quizgamearc.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var loginBinding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        loginBinding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = loginBinding.root
         setContentView(R.layout.activity_login)
+
+        loginBinding.btnSignIn.setOnClickListener {
+
+        }
+
+        loginBinding.btnGoogleSignIn.setOnClickListener {
+
+        }
+
+        loginBinding.tvSignUp.setOnClickListener {
+
+        }
+
+        loginBinding.tvForgotPassword.setOnClickListener {
+
+        }
+
+
+
     }
 }
